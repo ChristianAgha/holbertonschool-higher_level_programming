1935dc5745b7d830a5912754b5c30e03867bd85a
Christian Agha <christianagha@gmail.com>
2017-05-05 04:17:19 +0000

    pep8 fixes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1, sys
+    import calculator_1
+    import sys
+
     operators = ["+", "-", "*", "/"]
 
     if len(sys.argv) == 4:
+
         a, op, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+
         if op in operators:
             if sys.argv[2] is "+":
                 print("{:d} {:s} {:d} = {:d}".format(a, op, b, a + b))
+
             if sys.argv[2] is "-":
                 print("{:d} {:s} {:d} = {:d}".format(a, op, b, a - b))
+
             if sys.argv[2] is "*":
                 print("{:d} {:s} {:d} = {:d}".format(a, op, b, a * b))
+
             if sys.argv[2] is "/":
                 print("{:d} {:s} {:d} = {:d}".format(a, op, b, int(a / b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
+
     else:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
