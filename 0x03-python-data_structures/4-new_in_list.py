59bbf3a2f038a78ec0e45c21ab6299785102acfa
Christian Agha <christianagha@gmail.com>
2017-05-10 03:48:20 +0000

    changed index check

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     if my_list is None:
         return
     new_list = list(my_list)
-    if idx >= len(my_list):
+    if abs(idx) >= len(my_list):
         return new_list
     else:
         new_list[idx] = element
