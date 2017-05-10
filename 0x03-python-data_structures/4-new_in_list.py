c6d09149cbb5770fc77e8f9b481e2a8cc02ee4b1
Christian Agha <christianagha@gmail.com>
2017-05-10 03:40:44 +0000

    added empty list check

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
     new_list = list(my_list)
     if idx >= len(my_list):
         return new_list
