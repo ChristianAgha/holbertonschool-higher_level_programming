62c40b6dc2966f7dcd78032a5de69a23feee4b1f
Christian Agha <christianagha@gmail.com>
2017-05-10 03:45:19 +0000

    changed wrong index check, becasue index of list can be negative! who knew?

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None or idx < 0 or idx >= len(my_list):
+    if my_list is None or idx >= len(my_list):
         return
     new_list = list(my_list)
     if idx >= len(my_list):
