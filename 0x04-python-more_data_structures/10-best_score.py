77cf26141572677900e3d1a404e0eb2f66d068d4
Christian Agha <christianagha@gmail.com>
2017-05-12 04:49:08 +0000

    added key initialization

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict is None:
-        return("None")
+        return None
     highest_score = 0
+    key = None
     for k, v in my_dict.items():
         if v > highest_score:
             highest_score = v
