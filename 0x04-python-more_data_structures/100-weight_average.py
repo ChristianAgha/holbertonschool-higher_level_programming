6d1d55fbba9603abb4e724f0fdc2bd0df9a77a31
Christian Agha <christianagha@gmail.com>
2017-05-12 04:56:43 +0000

    syntax change

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None or my_list is []:
+    if my_list is None or my_list == []:
         return 0
     score = weight = 0
     for x, y in my_list:
