ba05ee955fcd5faa79897151892d21eed0d1830e
Christian Agha <christianagha@gmail.com>
2017-05-12 04:54:10 +0000

    adding prob #12

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    score = weight = 0
+    for x, y in my_list:
+        score += x * y
+        weight += y
+    return(score / weight)
