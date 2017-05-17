cc992b847a1f117daa145ea4bb33dc01d9d3467e
Christian Agha <christianagha@gmail.com>
2017-05-17 02:31:50 +0000

    adding number 5

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+This module contains a function 'text_indentation' that prints
+text it's given and adds 2 \n charachters after any ., ?, and :
+"""
+
+
+def text_indentation(text):
+    """
+    This functions takes text input and prints it out but adding 2 \n
+    charachters after every ., ? and :. It also raises an error if the
+    input was not a string.
+    """
+    special_chars = ['.', '?', ':']
+    skip_front_space = 0
+    if isinstance(text, str):
+        for i, char in enumerate(text):
+            if skip_front_space == 0 and char == ' ':
+                continue
+            skip_front_space = 1
+            if char == ' ' and text[i - 1] not in special_chars:
+                print(char, end='')
+            if char != ' ':
+                print(char, end='')
+
+            if char in special_chars:
+                print('\n\n', end='')
+                skip_front_space = 0
+    else:
+        raise TypeError("text must be a string")
