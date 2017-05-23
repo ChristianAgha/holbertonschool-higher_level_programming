a34ba73400ad563c3a0671156a9e7cfcfde171d8
Christian Agha <christianagha@gmail.com>
2017-05-23 04:45:52 +0000

    adding prob #3

---
+++ 0x07-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class """
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self._Square__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self._Square__size ** 2
