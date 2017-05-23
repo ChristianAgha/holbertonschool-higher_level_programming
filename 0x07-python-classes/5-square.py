1943934eee46652b41e3de9d41d02d575457ca90
Christian Agha <christianagha@gmail.com>
2017-05-23 06:33:32 +0000

    adding prob #5

---
+++ 0x07-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Square class """
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    # This is the getter
+    @property
+    def size(self):
+        return self.__size
+
+    # This is the setter
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    # function that returns area
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
