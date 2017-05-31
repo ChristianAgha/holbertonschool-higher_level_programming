b7735fd7e95a9750f73eae8ccb13778995930d32
Christian Agha <christianagha@gmail.com>
2017-05-31 05:15:34 +0000

    adding problem #12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""My Integer Module"""
+
+
+class MyInt(int):
+    """MyInt class"""
+
+    def __init__(self, num1):
+        """initialization"""
+        self.__num1 = num1
+
+    def __eq__(self, num2=0):
+        """overriding =="""
+        if (self.__num1 == num2):
+            return False
+        else:
+            return True
+
+    def __ne__(self, num2):
+        """overriding !="""
+        if (self.__num1 == num2):
+            return True
+        else:
+            return False
