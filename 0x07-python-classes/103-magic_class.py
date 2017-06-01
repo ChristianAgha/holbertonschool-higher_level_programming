6225bb9e2e6cefb49d8e0fe8f85dde75d9a21059
Christian Agha <christianagha@gmail.com>
2017-06-01 09:03:05 +0000

    added radius=0 to init func declaration

--- 0x07-python-classes/103-magic_class.py
+++ 0x07-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """class MagicClass"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """init def"""
         self._MagicClass__radius = 0
         if type(radius) is not int and type(radius) is not float:
