37a7e00a6f7f19c83bb29019c68fb0a87313d506
Christian Agha <christianagha@gmail.com>
2017-06-04 05:06:08 +0000

    adding prob #8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create object from a JSON file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a "JSON file"""
+    with open(filename, encoding='utf-8') as a_file:
+        return json.loads(a_file.read())
