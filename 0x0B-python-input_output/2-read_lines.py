489a4dfb162ac283db5b370cfb9c1de2a694fdfa
Christian Agha <christianagha@gmail.com>
2017-06-04 03:03:37 +0000

    fixed output for input larger than the number of lines in the file

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,8 +6,7 @@ def read_lines(filename="", nb_lines=0):
     """a function that reads n lines of a text file"""
     with open(filename, encoding='utf-8') as a_file:
         if nb_lines > 0:
-            lines = a_file.readlines()
             for i in range(nb_lines):
-                print(lines[i], end='')
+                print(a_file.readline(), end='')
         else:
             print(a_file.read(), end='')
