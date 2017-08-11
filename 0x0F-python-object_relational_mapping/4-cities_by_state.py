b5065ed62e4fa2359fdd26f3cec6f612945076f8
Christian Agha <christianagha@gmail.com>
2017-08-10 22:57:07 -0700

    adding problem #4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all cities from a database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.Connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("""
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id=states.id
+    ORDER BY cities.id ASC""")
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
