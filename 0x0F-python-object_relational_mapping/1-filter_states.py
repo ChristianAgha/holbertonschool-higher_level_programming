6dd7a6cf9bfda97ad528cdab41c587dad57af872
Christian Agha <christianagha@gmail.com>
2017-08-10 21:26:05 -0700

    added charset definition and removed ordering

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,10 @@ from sys import argv
 if __name__ == "__main__":
 
     db = MySQLdb.Connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3],  charset="utf8")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%'")
 
     states = cur.fetchall()
     for state in states:
