f368ae6d42c6ba57faf6892bbe934914e6dbd63e
Christian Agha <christianagha@gmail.com>
2017-08-10 21:40:36 -0700

    adding problem #2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+lists all states from a database
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.Connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name =\
+    '{}'".format(argv[4]))
+
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
