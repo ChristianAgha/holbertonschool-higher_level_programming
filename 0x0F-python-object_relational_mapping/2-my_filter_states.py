707b2d11afa39f4cec9194e3540b4062a3a9b82d
Christian Agha <christianagha@gmail.com>
2017-08-10 21:53:19 -0700

    added BINARY to query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3], charset="utf8")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name =\
+    cur.execute("SELECT * FROM states WHERE BINARY states.name =\
     '{}'".format(argv[4]))
 
     states = cur.fetchall()
