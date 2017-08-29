5449ce45b15cfa9dd005d763bd01f4ddb6c0f9ff
Christian Agha <christianagha@gmail.com>
2017-08-28 23:04:45 -0700

    added with statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,7 +3,6 @@
 Takes in a URL, sends a request to the URL & displays the body of the response
 """
 import urllib.request
-import urllib.parse
 import sys
 
 
@@ -12,9 +11,8 @@ if __name__ == "__main__":
     func definition
     """
     try:
-        req = urllib.request.Request(sys.argv[1])
-        response = urllib.request.urlopen(req)
-        html = response.read().decode('utf-8')
-        print(html)
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read().decode('utf-8')
+            print(html)
     except urllib.error.HTTPError as err:
         print("Error code:", err.code)
