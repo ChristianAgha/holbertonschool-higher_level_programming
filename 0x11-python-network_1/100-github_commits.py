495dc32fb2ad5061b18a19a4071188436549fbbb
Christian Agha <christianagha@gmail.com>
2017-08-29 01:27:12 -0700

    added an if to make sure the request was proper

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,15 +12,12 @@ if __name__ == "__main__":
     func def
     """
     u = "https://api.github.com/repos/" + argv[2] + "/" + argv[1] + "/commits"
-    try:
-        r = requests.get(u)
-        res = r.json()
-        com_lst = []
-        for item in res:
+    r = requests.get(u).json()
+    com_lst = []
+    if (r):
+        for item in r:
             sha = item["sha"]
             name = item["commit"]["author"]["name"]
             com_lst.append([sha, name])
         for n in range(10):
             print("{}: {}".format(com_lst[n][0], com_lst[n][1]))
-    except:
-        print("None")
