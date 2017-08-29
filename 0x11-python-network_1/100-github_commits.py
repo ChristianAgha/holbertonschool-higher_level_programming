bb9e11dc952876d4a313aa107f7e25238794feeb
Christian Agha <christianagha@gmail.com>
2017-08-29 01:13:44 -0700

    adding problem #11

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    func def
+    """
+    u = "https://api.github.com/repos/" + argv[2] + "/" + argv[1] + "/commits"
+    try:
+        r = requests.get(u)
+        res = r.json()
+        com_lst = []
+        for item in res:
+            sha = item["sha"]
+            name = item["commit"]["author"]["name"]
+            date = item["commit"]["author"]["date"]
+            com_lst.append([date, sha, name])
+        for n in range(10):
+            print("{}: {}".format(com_lst[n][1], com_lst[n][2]))
+    except:
+        print("None")
