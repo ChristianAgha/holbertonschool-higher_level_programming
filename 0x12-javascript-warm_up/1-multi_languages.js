819b95bd9166047029f8ccfa67cfc33c2dea2ae7
Christian Agha <christianagha@gmail.com>
2017-09-19 16:36:24 -0700

    adding problem #1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints statements
+const myArray = new Array(3);
+myArray[0] = 'C is fun';
+myArray[1] = 'Python is cool';
+myArray[2] = 'Javascript is amazing';
+for (let i = 0; i < 3; i++) {
+  console.log(myArray[i]);
+}
