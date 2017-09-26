1e7003b2cc83d65f906fce97bdd275d19a6f1cd8
Christian Agha <christianagha@gmail.com>
2017-09-26 15:21:16 -0700

    adding problem #6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Defines Square class
+
+const Square = require('./5-square').Square;
+
+Square.prototype.charPrint = function (char) {
+  if (char === undefined) {
+    char = 'X'
+  }
+  for (let i = 0; i < this.height; i++) {
+    console.log(char.repeat(this.width));
+    }
+};
+
+module.exports.Square = Square;
