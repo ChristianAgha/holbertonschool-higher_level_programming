6cc9c10c2a82d0f5a9b775626b035ef3019410c5
Christian Agha <christianagha@gmail.com>
2017-09-26 15:22:21 -0700

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,11 +5,11 @@ const Square = require('./5-square').Square;
 
 Square.prototype.charPrint = function (char) {
   if (char === undefined) {
-    char = 'X'
+    char = 'X';
   }
   for (let i = 0; i < this.height; i++) {
     console.log(char.repeat(this.width));
-    }
+  }
 };
 
 module.exports.Square = Square;
