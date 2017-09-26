f94b4ae2e3d27e9435244e25d9b7921bc7424e31
Christian Agha <christianagha@gmail.com>
2017-09-26 14:55:31 -0700

    adding prob #5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Defines Square class
+
+const Rectangle = require('./4-rectangle').Rectangle;
+
+function Square (size) {
+  Rectangle.call(this, size, size);
+}
+
+exports.Square = Square;
