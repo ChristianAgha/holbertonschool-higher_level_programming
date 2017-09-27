a3475a18bd95ce2c0ddb7763d70eb73f74cdacc9
Christian Agha <christianagha@gmail.com>
2017-09-26 22:05:59 -0700

    adding problem #4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Prints the # of movies where "Wedge Antilles" is present
+
+const apiUrl = process.argv[2];
+const request = require('request');
+let count = 0;
+
+request(apiUrl, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const jBody = JSON.parse(body);
+    for (let i = 0; i < jBody.results.length; i++) {
+      for (let j = 0; j < jBody.results[i].characters.length; j++) {
+        let character = jBody.results[i].characters[j];
+        let wedgeAntilles = 'https://swapi.co/api/people/18/';
+        if (character === wedgeAntilles) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
